b42c7e1d4ee49ca42f51d7ea5ded7bddbc7e57bc
James Smith <matthew02@users.noreply.github.com>
2020-01-05 15:36:29 -0600

    Problem 4 solution

---
+++ p0004.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Project Euler Problem 4 - Largest palindrome product
+
+Find the largest palindrome made from the product of two three-digit numbers.
+https://projecteuler.net/problem=4
+
+Usage:
+    python3 p0004.py [number]
+"""
+
+from typing import Iterator
+
+
+def largest_palindrome_product() -> int:
+    """Finds the largest palindromic product of two three-digit numbers.
+
+    Returns:
+        The largest product.
+    """
+    return max(generate_palindromes())
+
+def generate_palindromes() -> Iterator[int]:
+    """Generates a list of palindromes.
+
+    Finds all numbers which are the product of two three-digit numbers and
+    are palindromes.
+
+    Yields:
+        An iterator of all palindromes found.
+    """
+    for i in range(100, 1000):
+        for j in range(i, 1000):
+            product = i * j
+            if str(product) == str(product)[::-1]:
+                yield product
+
+def main():
+    """Prints the largest palindromic product of two three-digit numbers."""
+    print(largest_palindrome_product())
+
+if __name__ == '__main__':
+    main()
