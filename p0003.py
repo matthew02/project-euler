c94d9b507475caa6b400fb9f1130c041aab2f1a9
James Smith <matthew02@users.noreply.github.com>
2020-01-04 23:20:01 -0600

    Problem 3 solution

---
+++ p0003.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Problem 3 from Project Euler.
+
+Find the largest prime factor of a given number.
+https://projecteuler.net/problem=3
+
+Usage:
+    python3 p0003.py [number]
+"""
+
+import sys
+
+from math import sqrt
+from typing import Iterator
+
+
+def main(num: int):
+    """Prints the largest prime factor of a number.
+
+    Args:
+        num: The number to factor.
+    """
+    print(largest_prime_factor(num))
+
+def largest_prime_factor(num: int) -> int:
+    """Finds the largest prime factor of a given number.
+
+    Args:
+        num: The number to be factored.
+
+    Returns: The largest prime factor.
+    """
+    largest_prime = 0
+
+    stop = int(sqrt(num)) + 1
+    primes = generate_prime_numbers(0, stop)
+
+    for prime in primes:
+        if num % prime == 0:
+            largest_prime = prime
+
+    return largest_prime
+
+def generate_prime_numbers(start: int, stop: int) -> Iterator[int]:
+    """Generate a list of prime numbers.
+
+    Generates a list of all prime numbers in the range start (inclusive) to
+    stop (inclusive) using the Sieve of Eratosthenes algorithm.
+    https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+
+    Args:
+        start: The lower bound of the integer sequence to search for primes.
+        stop: The upper bound of the integer sequence to search for primes.
+
+    Returns: An iterator of all primes in the range [start: stop]
+    """
+    is_prime = [True for i in range(stop + 1)]
+
+    num = 2
+    while(num * num <= stop):
+        if (is_prime[num]):
+            for multiple in range(num * 2, stop, num):
+                is_prime[multiple] = False
+        num += 1
+
+    for num in range(2, stop + 1):
+        if num >= start and is_prime[num]:
+            yield num
+
+if __name__ == '__main__':
+    main(int(sys.argv[1]))
