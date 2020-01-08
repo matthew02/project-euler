d7de540d1d08ec3d7dc308f4d01efdc9a236d86d
James Smith <matthew02@users.noreply.github.com>
2020-01-07 22:45:45 -0600

    Problem 8 solution

---
+++ p0008.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Problem 8 from Project Euler.
+
+Find the n adjacent digits in the given number that have the greatest product.
+
+https://projecteuler.net/problem=8
+
+Usage:
+    python3 p0008.py [filename] [number]
+
+    [filename] points to a file containing the numerical sequence with or
+    without newline characters
+"""
+
+import numpy
+import sys
+
+from typing import List
+
+
+def largest_product_in_a_series(sequence: List, n: int) -> int:
+    """Finds the largest product of n consecutive numbers in sequence."""
+    largest = 0
+    for start in range(len(sequence) - n):
+        stop = start + n
+        largest = max(largest, numpy.prod(sequence[start:stop]))
+    return largest
+
+def main(fname: str, length: int):
+    """Prints the solution."""
+    with open (fname, 'r') as myfile:
+        data = [*map(int, myfile.read().replace('\n', ''))]
+    print(largest_product_in_a_series(data, length))
+
+if __name__ == '__main__':
+    main(sys.argv[1], int(sys.argv[2]))
+
