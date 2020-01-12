bfac110fd3f7ddff277521f1f81b5a50ff2f0f7a
James Smith <matthew02@users.noreply.github.com>
2020-01-11 20:33:05 -0600

    Problem 9 solution

--- p0009.py
+++ p0009.py
@@ -12,68 +12,40 @@ Usage:
 
 import sys
 
-from numpy import gcd
-from typing import List
+from typing import Iterator, List
+
+from euler import *
 
 
-def special_pythagorean_triplet(n: int) -> List:
-    """Finds a pythagorean triplet whose numbers sum to n."""
-    n2 = n / 2
-    stop = int(n2 ** 0.5 - 1)
-    for m in range(2, stop):
-        if n2 % m == 0:
-            nm = n2 // m
-            while nm % 2 == 0:
-                nm //= 2
-            if m % 2 == 1:
-                k = m + 2
-            else:
-                k = m + 1
-            while k < 2 * m and k <= nm:
-                if nm % k == 0 and gcd(k, m) == 1:
-                    d = n2 // (k * m)
-                    n = k - m
-                    a = d * (m ** 2 - n ** 2)
-                    b = 2 * d * m * n
-                    c = d * (m ** 2 + n ** 2)
-                    return([a, b, c])
-                k += 1
-
-    for x in range(1, n):
-        for y in range(1, n):
-            z = (x ** 2 + y ** 2) ** 0.5
-            if sum([x, y, z]) == n:
-                return [x, y, z]
-
-def special_pythagorean_triplet_old(n: int) -> List:
-    """Finds a pythagorean triplet whose numbers sum to n."""
-    for x in range(1, n):
-        for y in range(1, n):
-            z = (x ** 2 + y ** 2) ** 0.5
-            if sum([x, y, z]) == n:
-                return [x, y, z]
-
-def special_pythagorean_triplet_x(n: int) -> List:
-    """Finds a pythagorean triplet whose numbers sum to n."""
-    a = 3
-    while True:
-        b = (a ** 2 + 1) / 2 - 1
-        c = b + 1
-        s = sum([a, b, c])
-        print(f'a = {a}, b = {b}, c = {c}')
-        if s == n:
+def special_pythagorean_triplet(n: int) -> List[int]:
+    """Finds a Pythagorean triplet whose sum is equal to n."""
+    for a, b, c in get_dicksons_triplets(n):
+        if a + b + c == n:
             return [a, b, c]
-        if s > n:
-            return None
-        a += 2
+
+def get_dicksons_triplets(stop: int) -> Iterator[List[int]]:
+    """Generates Pythagorean triplets.
+
+    Uses Dickson's method, for all values of r from 1 to stop.
+
+    To find integer solutions to a^2 + b^2 = c^2...
+    Take positive integers r, s, and t, where r^2 = 2st is a perfect square...
+    Then a = r + s, b = r + t, c = r + s + t
+
+    https://en.wikipedia.org/wiki/Formulas_for_generating_Pythagorean_triples#Dickson's_method
+    """
+    for r in range(1, stop):
+        for s, t in get_factor_pairs(r ** 2 // 2):
+            yield [r + s, r + t, r + s + t]
 
 def main(num: int):
     """Prints the solution."""
     triplet = special_pythagorean_triplet(num)
-    if triplet is None:
+    if triplet == None:
         print('None found.')
     else:
-        print(triplet[0] * triplet[1] * triplet[2])
+        #print(triplet)
+        print(product(triplet))
 
 if __name__ == '__main__':
     main(int(sys.argv[1]))
