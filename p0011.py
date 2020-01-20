2856d504853b1b65c3f264b4ca19fdd075074f45
James Smith <matthew02@users.noreply.github.com>
2020-01-19 20:20:33 -0600

    Problem 11 solution, refactor

--- p0011.py
+++ p0011.py
@@ -12,11 +12,9 @@ Usage:
 """
 
 import numpy
-import operator
 import sys
 
-from functools import reduce
-from itertools import accumulate, count, islice, repeat, takewhile
+from itertools import count, islice
 from math import prod
 from typing import List
 
@@ -28,16 +26,16 @@ def largest_product_in_a_grid(grid: List[List[int]], num: int) -> int:
 
     for y in range(len(grid)):
         for x in range(len(grid[0])):
-            prod = lambda dx, dy: product(grid, [x, y], [dx, dy], num)
+            product = lambda dx, dy: grid_product(grid, [x, y], [dx, dy], num)
             largest = max([largest,
-                           prod(1, 0),      # Horizontal
-                           prod(0, 1),      # Vertical
-                           prod(1, 1),      # Diagonal down
-                           prod(1, -1)])    # Diagonal up
+                           product(1, 0),      # Horizontal
+                           product(0, 1),      # Vertical
+                           product(1, 1),      # Diagonal down
+                           product(1, -1)])    # Diagonal up
 
     return largest
 
-def product(grid: List[List[int]], start: List[int], direction: List[int], num: int) -> int:
+def grid_product(grid: List[List[int]], start: List[int], direction: List[int], num: int) -> int:
     """Calculate the product of a sequence of numbers in a grid.
 
     Args:
@@ -51,27 +49,28 @@ def product(grid: List[List[int]], start: List[int], direction: List[int], num: 
         would overflow the grid boundaries.
     """
     x, y = start
-
-    w, h = len(grid[0]) - 1, len(grid) - 1
-    end_x, end_y = x + num * direction[0], y + num * direction[1]
-    if x < 0 or end_x < 0 or x > w or end_x > w or\
-       y < 0 or end_y < 0 or y > h or end_y > h:
-        return 0
-
-    #prod = 1
-    #dx, dy = direction
-    #for _ in range(num):
-    #    prod *= grid[y][x]
-    #    x += dx
-    #    y += dy
-    #return prod
-
     dx, dy = direction
-    return product(grid[y + dy * i][x + dx * i] for i in range(num))
 
-    #x_coords = islice(count(x, direction[0]), 0, num)
-    #y_coords = islice(count(y, direction[1]), 0, num)
-    #return prod(grid[dy][dx] for dx, dy in zip(x_coords, y_coords))
+    try:
+        # Make sure the sequence doesn't start or end to the left or above the grid
+        if x < 0 or y < 0 or x + num * dx < 0 or y + num * dy < 0:
+            raise IndexError
+
+        #product = 1
+        #for _ in range(num):
+        #    product *= grid[y][x]
+        #    x += dx
+        #    y += dy
+        #return product
+
+        return prod(grid[y + dy * i][x + dx * i] for i in range(num))
+
+        #x_coords = islice(count(x, direction[0]), 0, num)
+        #y_coords = islice(count(y, direction[1]), 0, num)
+        #return prod(grid[dy][dx] for dx, dy in zip(x_coords, y_coords))
+
+    except IndexError:
+        return 0
 
 def main(fname: str, num: int):
     """Prints the solution."""
