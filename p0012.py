8ebdf7695932a282a162cb69822cbc4edf57852f
James Smith <matthew02@users.noreply.github.com>
2020-01-27 22:16:51 -0600

    Problem 12 solution, updated to work with euler.py update

--- p0012.py
+++ p0012.py
@@ -10,13 +10,12 @@ Usage:
 
 import sys
 
-
 from collections import Counter as counter
-from itertools import accumulate, count, dropwhile
 from math import prod
+from itertools import dropwhile, accumulate, count
 from typing import Iterator
 
-from euler import decompose
+from euler import decompose, get_primes
 
 
 def count_factors(num: int) -> int:
@@ -24,14 +23,18 @@ def count_factors(num: int) -> int:
     exponents = counter(decompose(num)).values()
     return prod(e + 1 for e in exponents)
 
-def triangle_numbers() -> Iterator:
-    """Generates the sequence of triangle numbers."""
+def get_triangle_numbers(n: int=1) -> Iterator[int]:
+    """Generates the sequence of triangle numbers starting with the nth."""
     return accumulate(count())
 
 def euler12(num: int) -> int:
     """Calculates the first triangle number to have more than num divisors."""
-    test_number = lambda n: count_factors(n) < num
-    return next(dropwhile(test_number, triangle_numbers()))
+    primes = get_primes()
+    for _ in range(1000):
+        trash = next(primes)
+
+    test = lambda n: count_factors(n) < num
+    return next(dropwhile(test, get_triangle_numbers()))
 
 def main(num: str):
     """Prints the solution."""
