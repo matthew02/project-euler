6fbf3a4ab12f4671c70fc268fbf298264fcf7e49
James Smith <matthew02@users.noreply.github.com>
2020-06-26 14:25:27 -0500

    Problem 15 solution.

---
+++ p015.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+"""Problem 14 from Project Euler: Lattice Paths
+
+Determine the number of NE routes in an n x n grid.
+
+Usage:
+    python3 p015.py [num]
+"""
+
+import sys
+
+from math import factorial
+
+
+def lattice_paths(n: int) -> int:
+    k = 2 * n
+    return factorial(k) // (factorial(n) * factorial(k - n))
+
+def euler15(num: int) -> int:
+    """Calculates the number of NE routes in an n x n grid."""
+    return lattice_paths(num)
+
+def main(num: str) -> None:
+    """Prints the solution."""
+    print(f'The number of NE routes in a {num} x {num} grid is {euler15(int(num))}.')
+
+if __name__ == '__main__':
+    main(sys.argv[1])
+
